5f5cdd5a3890dc790321bc3acd39e0c0d7149497
chenshaoze <chenshaoze2009@gmail.com>
2017-03-02 18:59:32 +0800

    remove some useless code

--- app/reducers/studentManageReducer.js
+++ app/reducers/studentManageReducer.js
@@ -1,32 +1,5 @@
 import { SUCCESS } from '../actions/studentManageActions'
 
-const loadStudentInfo = (state) => {
-	return {...state, 
-		students:[
-				{	
-					name:"张三",
-					college:"软件学院",
-					card:"123456"
-				},
-				{
-					name:"张三",
-					college:"软件学院",
-					card:"123456"
-				},
-				{
-					name:"张三",
-					college:"软件学院",
-					card:"123456"
-				},
-				{
-					name:"张三",
-					college:"软件学院",
-					card:"123456"
-				}
-			]
-		}
-}
-
 const studentManageReducer = (state={students:[]}, action) => {
 	console.log(action)
 	switch(action.type) {
